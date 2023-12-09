5104eefd851c65598d3b9027ffe40593e0e711a1
aritabryan <147879047+aritabryan@users.noreply.github.com>
2023-12-08 21:32:54 -0600

    Add files via upload

---
+++ Lab9P1_BryanArita/src/lab9p1_bryanarita/Lab9P1_BryanArita.java
@@ -0,0 +1,17 @@
+//fila 2 asiento 2
+package lab9p1_bryanarita;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lab9P1_BryanArita {
+
+    public Scanner duki = new Scanner(System.in);
+    public Random rndm = new Random();
+
+    public static void main(String[] args) {
+        Frameinicial gui = new Frameinicial();
+        gui.setVisible(true);
+    }
+
+}
